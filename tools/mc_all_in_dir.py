3fcb0d8a48ce93d084d2fff3cfb7b24b9de8b50b
Kazsufumi Ikemoto <kazufumi.ikemoto@istellartech.com>
2019-02-18 03:26:45 +0000

    add python tools for monte_carlo sim.

---
+++ tools/mc_all_in_dir.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+from __future__ import *
+import os, sys
+import subprocess as sp
+
+if __name__=="__main__":
+    if(len(sys.argv) == 1) :
+        print("usage: python", sys.argv[0], "TAGET_DIR", "[N_ARRAY]")
+        exit(1)
+
+    parent_dir = sys.argv[1]
+    if(len(sys.argv) > 2) :
+        n_array = int(sys.argv[2])
+        i_array = int(os.getenv("AWS_BATCH_JOB_ARRAY_INDEX"))
+    else :
+        n_array = 1
+        i_array = 0
+
+    is_aws = parent_dir.startswith("s3://")
+    if is_aws:
+        prefix = "aws s3 "
+    else :
+        prefix = ""
+        parent_dir = os.path.abspath(parent_dir)
+
+    curr_dir = os.path.abspath(".")
+    path_tsio = os.getenv("PATH_OPENTSIO", "/usr/local/OpenTsiolkovsky/bin")    # default path
+    os.chdir(path_tsio)
+
+    child_dirs = sp.check_output("{0}ls -F {1} | grep / | sed 's/\///g'".format(prefix, parent_dir), shell=True).decode("utf-8").strip().split("\n")
+
+    for j in range(i_array, len(child_dirs), n_array):
+        ch_fullpath = "{0}/{1}".format(parent_dir, child_dirs[j])
+        try :
+            ret = os.system("{0}ls {1}/raw/inp/mc.json".format(prefix, ch_fullpath))
+            if ret == 0: # mc.json is exist
+                os.system("python ./monte_carlo.py {0}".format(ch_fullpath))
+        except :
+            pass
+
+    os.chdir(curr_dir)
+
---
+++ tools/mkdirs_cutoff.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from __future__ import *
+import json
+import os, sys
+import numpy as np
+
+if __name__=="__main__":
+    if(len(sys.argv) < 4) :
+        print("usage: python", sys.argv[0], "TARGET_DIR", "T_MAX", "T_SPAN")
+        exit(1)
+    target_dir = sys.argv[1]
+    t_max = float(sys.argv[2])
+    t_span = float(sys.argv[3])
+
+    base_dir = "{}/base".format(target_dir)
+    cutoff_dir = "{}/cutoff".format(target_dir)
+
+    is_aws = target_dir.startswith("s3://")
+    if is_aws:
+        prefix = "aws s3 "
+    else :
+        prefix = ""
+
+    temp_dir = "./my_temp_dir_999"
+    os.system("mkdir {}".format(temp_dir)
+
+    for t in np.arange(t_span, t_max+t_span, t_span) :
+        t_dir = "{0}/{1:.2f}".format(cutoff_dir, t)
+
+        if not is_aws :
+            os.system("mkdir -p {}/raw/inp".format(t_dir))
+            os.system("mkdir -p {}/raw/output".format(t_dir))
+        os.system("{0}cp {1}/raw/inp {2}/raw/inp -r".format(prefix, base_dir, t_dir))
+
+        os.system("{0}cp {1}/raw/inp/*.json {2}".format(prefix, t_dir, temp_dir))
+        with open("{0}/mc.json".format(temp_dir)) as fp :
+            mc = json.load(fp)
+
+        mc["suffix"] = mc["suffix"] + "_{:.2f}".format(t)
+        with open("{0}/mc.json".format(temp_dir), "w") as fp :
+            json.dump(mc, fp, indent=4)
+
+        nomfile = mc["nominalfile"]
+        with open("{0}/{1}".format(temp_dir, nomfile)) as fp:
+            nom = json.load(fp)
+        
+        nom["stage1"]["thrust"]["forced cutoff time(time of each stage)[s]"] = t
+        with open("{0}/{1}".format(temp_dir, nomfile), "w") as fp:
+            json.dump(nom, fp, indent=4)
+
+        os.system("{0}mv {1}/*.json {2}/raw/inp".format(prefix, temp_dir, t_dir)
+
+    os.system("rm -rf {}".format(temp_dir))
+
