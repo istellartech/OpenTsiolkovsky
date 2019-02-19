dfa5feaddac76e633b64d837a71c70339b002458
KazufumiIkemoto <kazufumi.ikemoto@istellartech.com>
2019-02-19 18:45:53 +0900

    update to change ballistic coeff. with time

--- tools/mkdirs_cutoff.py
+++ tools/mkdirs_cutoff.py
@@ -4,6 +4,8 @@ from __future__ import print_function, division, unicode_literals, absolute_impo
 import json
 import os, sys
 import numpy as np
+import pandas as pd
+from scipy import interpolate
 
 if __name__=="__main__":
     if(len(sys.argv) < 4) :
@@ -23,7 +25,22 @@ if __name__=="__main__":
         prefix = ""
 
     temp_dir = "./my_temp_dir_999"
-    os.system(r"mkdir {}".format(temp_dir))
+    if not os.path.exists(temp_dir) : os.system(r"mkdir {}".format(temp_dir))
+
+    if is_aws :
+        os.system(r"{0}cp {1}/raw/inp {2} --recursive".format(prefix, t_dir, temp_dir))
+    else :
+        os.system(r"{0}cp {1}/raw/inp/* {2}".format(prefix, t_dir, temp_dir))
+    
+    with open("{0}/mc.json".format(temp_dir)) as fp :
+        mc = json.load(fp)
+    nomfile = mc["nominalfile"]
+    with open("{0}/{1}".format(temp_dir, nomfile)) as fp:
+        nom = json.load(fp)
+    df = pd.read_csv("{0}/output/{1}_dynamics_1.csv".format(temp_dir, nom["name(str)"]))
+    time_nom = df["time(s)"].values
+    mass_nom = df["mass(kg)"].values
+    mass_nom_at = interpolate.interp1d(time_nom, mass_nom)
 
     for t in np.arange(t_span, t_max+t_span, t_span) :
         t_dir = "{0}/{1:06.2f}".format(cutoff_dir, t)
@@ -52,7 +69,13 @@ if __name__=="__main__":
         with open("{0}/{1}".format(temp_dir, nomfile)) as fp:
             nom = json.load(fp)
         
-        nom["stage1"]["thrust"]["forced cutoff time(time of each stage)[s]"] = t
+        t_coff_nom = nom["stage1"]["thrust"]["forced cutoff time(time of each stage)[s]"]
+        beta_nom = nom["stage1"]["aero"]["ballistic coefficient(ballistic flight mode)[kg/m2]"]
+        t_coff_new = t
+        beta_new = beta_nom * mass_nom_at(t_coff_new) / mass_nom_at(t_coff_nom)
+
+        nom["stage1"]["thrust"]["forced cutoff time(time of each stage)[s]"] = t_coff_new
+        nom["stage1"]["aero"]["ballistic coefficient(ballistic flight mode)[kg/m2]"] beta_new
         with open("{0}/{1}".format(temp_dir, nomfile), "w") as fp:
             json.dump(nom, fp, indent=4)
 
