c2c857c052568e655d827524a6977db9cb69374b
A-Hayasaka <akira.hayasaka@istellartech.com>
2021-07-13 09:42:34 +0900

    fix stat_covariance.py
    fixed behaviour when landing_time or specific time is given as key_sample_point

--- bin/stat_covariance.py
+++ bin/stat_covariance.py
@@ -19,7 +19,7 @@ def read_data_points(arg):
     start_index = shou *  id_proc      + min(amari, id_proc)
     end_index   = shou * (id_proc + 1) + min(amari, id_proc + 1)
 
-    key_variable_names_all = []
+    key_variable_names_all = ["time(s)"]
     for sample_point in sample_points:
         key_variable_names_all.extend(sample_points[sample_point])        
     key_variable_names_all = sorted(set(key_variable_names_all),key=key_variable_names_all.index)
@@ -30,34 +30,34 @@ def read_data_points(arg):
         caseNo = i+1
         filename = input_file_template.format(caseNo)
 
-        time0 = pytime.time()
-
         os.system("aws s3 cp "+input_directory+filename+" . > /dev/null")
         if id_proc == 0: 
             print("{0:}/{1:}".format(caseNo, end_index))
         #os.system("cp data/"+filename+" .") ######## FOR DEBUG##################
 
         # format csv file to proper form
-        fp = open(filename)
+        try :
+          fp = open(filename)
+        except :
+          continue
         ft = open("tmp_{}.csv".format(id_proc),"w")
         for line in fp:
             ft.write(line.replace(",\n","\n"))
         fp.close()
         ft.close()
-
         #if id_proc == 0: 
         #    print("DOWNLOADED  ID#{:2d} CASE#{:5d} : {:f}seconds".format(id_proc,caseNo,pytime.time() - time0)) #for benchmark
 
         # fetch data
-        time0 = pytime.time()
-        df = pd.read_csv("tmp_{}.csv".format(id_proc),usecols=["time(s)"].extend(key_variable_names_all))
+        df = pd.read_csv("tmp_{}.csv".format(id_proc),usecols=key_variable_names_all)
+        
 
         for key_variable_name in key_variable_names_all:
 
             if dfs[key_variable_name].empty:
-                dfs[key_variable_name] = df.loc[:,["time(s)",key_variable_name]]
+                dfs[key_variable_name] = df.loc[:,key_variable_name]
             else:
-                dfs[key_variable_name] = pd.merge(dfs[key_variable_name], df.loc[:,["time(s)",key_variable_name]], on="time(s)", how="outer")
+                dfs[key_variable_name] = pd.concat([dfs[key_variable_name], df.loc[:,key_variable_name]], axis=1, join="outer")
 
         # remove temporary csv
         os.system("rm "+filename)
@@ -173,44 +173,40 @@ if __name__ == "__main__":
 #    callback = [read_data_points((id_proc, Nproc, input_directory, input_file_template, number_of_sample, Nfetch, sample_points))]
 
     print('loading complete. time: {:f} second'.format(pytime.time()-start_time))
+    
+    # join them
+    key_variable_names_all = ["time(s)"]
+    for sample_point in sample_points:
+        key_variable_names_all.extend(sample_points[sample_point])        
+    key_variable_names_all = sorted(set(key_variable_names_all),key=key_variable_names_all.index)
 
+    dfs = OrderedDict({k:pd.DataFrame() for k in key_variable_names_all})
+
+    time0 = pytime.time()
+    for key_variable_name in dfs.keys():
+        dfs[key_variable_name] = pd.concat(list(map(lambda x:x[key_variable_name], callback)), axis=1, join="outer")
+
+    time_m = pytime.time() - time0
+    #print("MERGE: {:f} seconds".format(time_m)) #for benchmark
 
     for key_sample_point in sample_points.keys():
 
         df_out = pd.DataFrame()
-       
-        # join them
         key_variable_names = sample_points[key_sample_point]
-        dfs = OrderedDict({k:pd.DataFrame() for k in key_variable_names})
-
-        for id_proc in range(Nproc):
-
-            time0 = pytime.time()
-
-            for key_variable_name in dfs.keys():
-
-                df_proc = callback[id_proc][key_variable_name]
-
-                if dfs[key_variable_name].empty:
-                    dfs[key_variable_name] = df_proc
-                else:
-                    dfs[key_variable_name] = pd.merge(dfs[key_variable_name], df_proc, on="time(s)", how="outer")
-
-            #print("MERGE: {}, {:f} seconds".format(id_proc,pytime.time()-time0)) #for benchmark            
 
         for key_variable_name in key_variable_names:
             
             time0 = pytime.time()
             
             if key_sample_point == "all":
-                df_src = dfs[key_variable_name].set_index("time(s)").T
+                df_src = dfs[key_variable_name].T
                 df_stat = df_src.apply(lambda x:fetch_stat(x, fetch_mode, number_of_sample, Nfetch, probability)).T
             elif key_sample_point == "landing_time":
-                df_src = dfs[key_variable_name].fillna(method='ffill').set_index("time(s)").T.iloc[-1,:]
-                df_stat = pd.DataFrame(fetch_stat(df_src, fetch_mode, number_of_sample, Nfetch, probability)).T
+                df_src = dfs[key_variable_name].fillna(method='ffill').T.iloc[:,-1]
+                df_stat = pd.DataFrame(fetch_stat(df_src, fetch_mode, number_of_sample, Nfetch, probability),columns=[key_sample_point]).T
             else:
-                df_src = dfs[key_variable_name].set_index("time(s)").T.loc[int(key_sample_point),:]
-                df_stat = pd.DataFrame(fetch_stat(df_src, fetch_mode, number_of_sample, Nfetch, probability)).T
+                df_src = dfs[key_variable_name].T.iloc[:,int(key_sample_point)]
+                df_stat = pd.DataFrame(fetch_stat(df_src, fetch_mode, number_of_sample, Nfetch, probability),columns=[int(key_sample_point)]).T
 
             df_stat.columns = [key_variable_name+"_high", key_variable_name+"_low"]
             
@@ -219,11 +215,12 @@ if __name__ == "__main__":
                 df_out = df_stat
             else:
                 df_out = df_out.join(df_stat,how="outer")
+
+           
+            #print("STAT : {}, {:f} seconds".format(key_variable_name,pytime.time()-time0)) #for benchmark
             
-            #print("STAT: {}, {:f} seconds".format(key_variable_name,pytime.time()-time0)) #for benchmark
-            
-        df_out.to_csv("./output/covariance_{}.csv".format(key_sample_point))
+        df_out.dropna(axis=0,how="all").to_csv("output/covariance_{}.csv".format(key_sample_point))
 
     os.system("aws s3 cp output s3://otmc/{0:s}/stat/output/ --exclude '*' --include 'covariance_*.csv' --recursive".format(otmc_mission_name))
 
-    print('calculation complete. time: {:f} second'.format(pytime.time()-start_time))
+    print('calculation complete. total time: {:f} second'.format(pytime.time()-start_time))
